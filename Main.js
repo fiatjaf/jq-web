44b02f1d25ef70c2c752ba2df6633f7f8117fc0d
fiatjaf <fiatjaf@gmail.com>
2017-05-17 14:14:06 +0000

    shareable urls.

--- Main.js
+++ Main.js
@@ -2,22 +2,59 @@ const createClass = require('create-react-class')
 const h = require('react-hyperscript')
 const CodeMirror = require('react-codemirror')
 const JSONPretty = require('react-json-pretty')
+const qs = require('qs')
 const jq = window.jq
 const debounce = require('debounce')
 
 require('codemirror/mode/javascript/javascript')
 
+const query = qs.parse(location.search.slice(1))
+
+const dflt = [
+  [`{
+  "values": [1, 2, 7, 23, 48, 26],
+  "label": "mouse",
+  "name": "gerald"
+}`, '"\\(.name), the \\(.label)," as $phrase | .values | reduce .[] as $v (0;.+$v) | $phrase + " has a value of \\(.)"'],
+  [`[{
+  "kind": "fruit",
+  "name": "banana"
+}, {
+  "kind": "sport",
+  "name": "tennis"
+}, {
+  "kind": "fruit",
+  "name": "strawberry"
+}, {
+  "kind": "fruit",
+  "name": "mango"
+}]`, 'reduce .[] as $item ({}; .[$item.kind] |= . + 1)'],
+  [`{
+  "a": {
+    "big": [
+      892173,
+      {
+        "nested": {
+          "json": {
+            "some key deep inside": "some value deep inside"
+          }
+        }
+      }
+    ]
+  }
+}`, '.a.big[1].nested.json']
+]
+
+let chosen = dflt[parseInt(Math.random() * dflt.length)]
+
 module.exports = createClass({
   displayName: 'Main',
   getInitialState () {
     return {
-      input: `{
-  "field": {
-    "internal": "object"
-  }
-}`,
-      filter: '.field',
-      output: null
+      input: query.i || chosen[0],
+      filter: query.f || chosen[1],
+      output: null,
+      error: null
     }
   },
 
@@ -44,6 +81,9 @@ module.exports = createClass({
             ])
           ]),
           h('.nav-center', [
+            h('a.nav-item', {
+              onClick: this.shareURL
+            }, 'share'),
             h('a.nav-item', {
               href: 'https://stedolan.github.io/jq/manual/',
               target: '_blank'
@@ -78,7 +118,9 @@ module.exports = createClass({
                 })
               ])
             ]),
-            h(JSONPretty, {json: this.state.output})
+            this.state.output
+            ? h(JSONPretty, {json: this.state.output})
+            : h('pre', [ h('code', this.state.error) ])
           ])
         ])
       ])
@@ -89,14 +131,18 @@ module.exports = createClass({
     this.setState(change, this.dcalc)
   },
 
+  shareURL (e) {
+    window.history.pushState(null, null, '?' + qs.stringify({i: this.state.input, f: this.state.filter}))
+  },
+
   calc () {
     var output
     try {
       let json = JSON.parse(this.state.input)
       output = jq(json, this.state.filter)
+      this.setState({output})
     } catch (e) {
-      output = e.message
+      this.setState({output: null, error: e.message})
     }
-    this.setState({output})
   }
 })
