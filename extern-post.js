a80d4bacbbc7c0d82a3716fc83e725ec7a4300da
Felipe Gasper <felipe@felipegasper.com>
2024-05-15 20:15:25 -0400

    fix web

--- extern-post.js
+++ extern-post.js
@@ -1,3 +1,8 @@
-module.exports = jq().then( module => {
-    return { json: module.json, raw: module.raw };
+jq = jq().then( module => {
+  return { json: module.json, raw: module.raw };
 } );
+
+if (typeof exports === 'object' && typeof module === 'object')
+  module.exports = jq;
+else if (typeof exports === 'object')
+  exports["jq"] = jq;
